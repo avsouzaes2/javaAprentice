fae29d47769ea97971b41587399a294854ed069d
Alexandre <avsouza.es@gmail.com>
2026-05-14 22:19:02 -0300

    feat(basics): add hello world and primitive types examples

---
+++ src/main/java/com/avsouzaes/aprendendojava/Basico/TiposPrimitivos.java
@@ -0,0 +1,14 @@
+
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+        double b = 55.0;
+        System.out.println(b);
+        float c = 10.0f;
+        System.out.println(c);
+        long d = 1000000000L;
+        System.out.println(d);
+    }
+}
