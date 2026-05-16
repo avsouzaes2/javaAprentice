a685ecf1a2403b984a5e5846ae082d37a8fdf3b9
Alexandre <avsouza.es@gmail.com>
2026-05-15 23:12:37 -0300

    feat(basico): add positive/negative number check example class

---
+++ src/main/java/com/avsouzaes/aprendendojava/Basico/ExmploPositivoNegativo.java
@@ -0,0 +1,11 @@
+public class ExmploPositivoNegativo {
+    public static void main(String[] args) {
+        int numero = 8;
+
+        if (numero >=0) {
+            System.out.println("O número é positivo");
+        } else {
+            System.out.println("O número é negativo");
+        }
+    }    
+}
