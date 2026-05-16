a367d0248a48104bcf227187f0fbc04e76bfeb0b
Alexandre <avsouza.es@gmail.com>
2026-05-15 23:15:36 -0300

    feat(basico java): add even-odd number check
    create a new Java class that checks if an integer is even or odd, using a hardcoded test value of 5

---
+++ src/main/java/com/avsouzaes/aprendendojava/Basico/ExemploParOuImpar.java
@@ -0,0 +1,10 @@
+public class ExemploParOuImpar {
+    public static void main(String[] args) {
+        int numero = 5;
+        if (numero % 2 == 0 ) {
+            System.out.println("O número é par");
+        } else {
+            System.out.println("O número é ímpar");
+        }
+    }    
+}
