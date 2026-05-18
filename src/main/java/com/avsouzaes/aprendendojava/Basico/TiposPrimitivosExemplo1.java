7a86216f9586bedd2cb3078d79b1318a8d247155
Alexandre <avsouza.es@gmail.com>
2026-05-17 21:24:14 -0300

    feat(basics): add TiposPrimitivosExemplo1 example show use of primitive and reference data types with sample student data and printed output, include both source and compiled artifact.

---
+++ src/main/java/com/avsouzaes/aprendendojava/Basico/TiposPrimitivosExemplo1.java
@@ -0,0 +1,22 @@
+public class TiposPrimitivosExemplo1 {
+	public static void main(String[] args) {
+		// aluno1
+		int codigoCadastro = 001;
+		String nomeAluno = "Alexandre";
+		char sexoAluno = 'M';
+		
+		// aluno2
+		int codigoCadastro2 = 002;
+		String nomeAluno2 = "Maria";
+		char sexoAluno2 = 'F';
+
+
+		System.out.println("Nome do Aluno: " + nomeAluno);
+		System.out.println("Sexo do Aluno: " + sexoAluno);
+		System.out.println("-----------------------------");
+		System.out.println("Nome do Aluno: " + nomeAluno2);
+		System.out.println("Sexo do Aluno: " + sexoAluno2);
+		
+
+	}
+}
